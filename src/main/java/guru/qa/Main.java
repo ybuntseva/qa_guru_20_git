842e9811c4e37e17c8d0b568af20c7f7e318224d
Yulia Buntseva <juliabuntseva@gmail.com>
2023-05-29 19:46:24 +0200

    message update

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,7 +3,7 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
 
-        String text = "Hello, qa.guru!!";
+        String text = "Hello, qa.guru! It's nice to see you!";
         System.out.println(text);
     }
 }
